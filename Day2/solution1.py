68c68e355b866700a809b056ad6ee545733693be
AlchemicalWizard <45638741+AlchemicalWizard@users.noreply.github.com>
2024-12-03 21:58:41 +0000

    feat(AoC): add solutions for days 1-3

---
+++ Day2/solution1.py
@@ -0,0 +1,99 @@
+
+
+def parse_data(filename):
+    file = open(filename)
+    lines = file.readlines()
+    
+    return lines
+
+def test(input, expected):
+    if(input != expected):
+        raise Exception("Test failed, expected " + str(expected) + " but received " + str(input))
+    print("test passed")
+    
+def check_report_part1(report):
+    values = [int(string) for string in report.replace("\n", "").split(" ")]
+    
+    status = (values[0] > values[1]) - (values[0] < values[1]) 
+    
+    if not status or abs(values[0] - values[1]) > 3:
+        return False
+    
+    for i, value in enumerate(values[2:]):
+        previousValue = values[i+1]
+        if (previousValue > value) - (previousValue < value) != status or abs(previousValue - value) > 3:
+            return False
+    return True
+
+def check_report_for_error(values):
+    status = (values[0] > values[1]) - (values[0] < values[1]) 
+    
+    if not status or abs(values[0] - values[1]) > 3:
+        return 0
+    
+    for i, value in enumerate(values[2:]):
+        previousValue = values[i+1]
+        if (previousValue > value) - (previousValue < value) != status or abs(previousValue - value) > 3:
+            return i+2
+    return -1
+
+def check_report_part2(report):
+    values = [int(string) for string in report.replace("\n", "").split(" ")]
+    
+    result = check_report_for_error(values)
+    
+    if result == -1:
+        return True
+    
+    for i in range(len(values)):
+        valuesCopy = values.copy()
+        valuesCopy.pop(i)
+        
+        newResult = check_report_for_error(valuesCopy)
+        
+        if newResult == -1:
+            return True
+        
+    return False
+
+def solve_a(file):
+    reports = parse_data(file)
+    
+    safeReports = 0
+    for report in reports:
+        if check_report_part1(report):
+            safeReports += 1
+        
+    return safeReports
+
+def solve_b(file):
+    reports = parse_data(file)
+    
+    safeReports = 0
+    for report in reports:
+        if check_report_part2(report):
+            safeReports += 1
+        
+    return safeReports
+
+
+def run_a():
+    test_result = solve_a("test.txt")
+
+    test(test_result, 2)
+
+    result = solve_a("data.txt")
+    print(result)
+
+
+def run_b():
+    test_result = solve_b("test.txt")
+
+    test(test_result, 4)
+
+    result = solve_b("data.txt")
+    print(result)
+
+
+run_a()
+run_b()
---
+++ Day3/solution1.py
@@ -0,0 +1,65 @@
+import re
+
+def parse_data(filename):
+    file = open(filename)
+    return file.read()
+
+def test(input, expected):
+    if(input != expected):
+        raise Exception("Test failed, expected " + str(expected) + " but received " + str(input))
+    print("test passed")
+    
+
+
+def solve_a(file):
+    string = parse_data(file)
+    matches = re.findall( r'mul\(\d{1,3},\d{1,3}\)', string)
+    
+    result = 0
+    
+    for match in matches:
+        match = match.replace("mul(", "").replace(')', "")
+        a, b = match.split(",")
+        
+        result += int(a) * int(b)
+        
+    return result
+
+
+def solve_b(file):
+    string = parse_data(file)
+    matches = re.findall( r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", string)
+
+    result = 0
+    
+    mulEnabled = True
+    for match in matches:
+        if match == "do()":
+            mulEnabled = True
+        elif match == "don't()":
+            mulEnabled = False
+        elif mulEnabled:
+            match = match.replace("mul(", "").replace(')', "")
+            a, b = match.split(",")
+        
+            result += int(a) * int(b)
+    return result
+
+def run_a():
+    test_result = solve_a("test.txt")
+
+    test(test_result, 161)
+
+    result = solve_a("data.txt")
+    print(result)
+
+def run_b():
+    test_result = solve_b("test2.txt")
+
+    test(test_result, 48)
+
+    result = solve_b("data.txt")
+    print(result)
+
+run_a()
+run_b()
